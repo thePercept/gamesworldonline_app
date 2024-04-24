308da84d6eab97cbb64e2502ea5514153d150961
Abhishek <dpercpet@gmail.com>
2024-04-24 23:36:38 +0530

    FInal Code

--- db_stuff/initiation.sql
+++ db_stuff/initiation.sql
@@ -10,4 +10,31 @@ CREATE TABLE IF NOT EXISTS `accounts` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+-- Create the games table
+CREATE TABLE IF NOT EXISTS `games` (
+    `id` VARCHAR(10) PRIMARY KEY,
+    `game_name` VARCHAR(255),
+    `year_launched` INT,
+    `game_rating` VARCHAR(10),
+    `base_price` INT,
+    `platform` VARCHAR(50),
+    `created_by` VARCHAR(100) DEFAULT 'admin@gamesworldonline.in'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Insert data into the games table
+INSERT INTO `games` (`id`, `game_name`, `year_launched`, `game_rating`, `base_price`, `platform`) VALUES
+('GWO_2001', 'Prince Of Persia', 2004, '4.5/10', 4200, 'PC'),
+('GWO_0032', 'Prince Of Persia', 2004, '4.5/10', 4200, 'PS2'),
+('GWO_1001', 'The Legend of Zelda: Breath of the Wild', 2017, '9.7/10', 4999, 'Nintendo Switch'),
+('GWO_4050', 'Red Dead Redemption 2', 2018, '9.5/10', 3499, 'PS4'),
+('GWO_7890', 'Grand Theft Auto V', 2013, '9.4/10', 1999, 'Xbox One'),
+('GWO_2345', 'Super Mario Odyssey', 2017, '9.6/10', 3999, 'Nintendo Switch'),
+('GWO_5001', 'The Witcher 3: Wild Hunt', 2015, '9.9/10', 2999, 'PC'),
+('GWO_7320', 'God of War', 2018, '9.8/10', 3499, 'PS4'),
+('GWO_8623', 'Call of Duty: Modern Warfare', 2019, '8.7/10', 2999, 'Xbox One'),
+('GWO_1520', 'Minecraft', 2011, '9.0/10', 1999, 'PS3'),
+('GWO_3567', 'FIFA 22', 2021, '8.9/10', 4499, 'PS5'),
+('GWO_9381', 'Among Us', 2018, '8.0/10', 299, 'Android'),
+('GWO_1123', 'Cyberpunk 2077', 2020, '7.2/10', 2999, 'PC'),
+('GWO_2810', 'Assassin\'s Creed Valhalla', 2020, '8.5/10', 3999, 'Xbox Series X'),
+('GWO_7777', 'Animal Crossing: New Horizons', 2020, '9.3/10', 4999, 'Nintendo Switch');
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
- 
+import pymysql
+
  
 app = Flask(__name__)
  
@@ -12,7 +13,7 @@ app.secret_key = 'your secret key'
  
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'AITkWu7p'
+app.config['MYSQL_PASSWORD'] = 'mypassword'
 app.config['MYSQL_DB'] = 'gameworlds_db'
  
  
@@ -36,8 +37,24 @@ def login():
             session['loggedin'] = True
             session['id'] = account['id']
             session['email'] = account['email']
-            msg = 'Logged in successfully ! USERNAME IS : {}'.format(account['username'])
-            return render_template('index.html', msg=msg)
+
+            # Inserting Games List
+
+            cursor.execute('SELECT * FROM games')
+            games = cursor.fetchall()
+            cart = session.get('cart', [])              
+
+            site_data ={
+                'userdata':{
+                    'username':account['username'],
+                    'email':account['email'],
+                },
+                'gamedata':games,
+                'cart':cart
+            }
+            return redirect(url_for('list_games'))    
+            
+
         else:
             print("Account not found ....")
             msg = 'Incorrect username / password !'
@@ -80,6 +97,119 @@ def register():
         else:
             msg = 'Please fill out the form!'
     return render_template('register.html', msg=msg)
+
+
+
+@app.route('/users')
+def list_users():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM accounts')
+    users = cursor.fetchall()
+    return render_template('users.html', users=users)
+
+
+@app.route('/games')
+def list_games():
+    email=None
+    print("Session is ",session)
+    if 'email' in session:
+        email = session['email']
+    print(email)
+
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM accounts WHERE email = %s', (email,))
+    account = cursor.fetchone()    
+
+    cursor.execute('SELECT * FROM games')
+    games = cursor.fetchall()
+    cart = session.get('cart', [])     
+
+         
+
+    site_data ={
+        'userdata':{
+            'username':account['username'],
+            'email':account['email'],
+        },
+        'gamedata':games,
+        'cart':cart
+    }
+    return render_template('games.html',site_data=site_data)
+
+
+
+def get_game_info(game_id):
+    try:
+        # Connect to the database
+        connection = pymysql.connect(
+            host=app.config['MYSQL_HOST'],
+            user=app.config['MYSQL_USER'],
+            password=app.config['MYSQL_PASSWORD'],
+            db=app.config['MYSQL_DB'],
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+        with connection.cursor() as cursor:
+            # Execute query to fetch game price
+            cursor.execute('SELECT id,base_price,game_name FROM games WHERE id = %s', (game_id,))
+            game = cursor.fetchone()
+
+            print("Getting game",game)
+
+            if game:
+                return game # Return the price if game found
+            else:
+                return None  # Return None if game not found
+
+    except pymysql.Error as err:
+        print("Error accessing database:", err)
+        return None
+
+    finally:
+        if 'connection' in locals():
+            connection.close()
+
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    print(request.json)
+    game_id = request.json.get('gameId') 
+    game_info = get_game_info(game_id)
+    print("GAME ID RECIEVED",game_id)
+    print("CART VALUE ",session)
+    print("CART VALUE ",game_info)
+    if 'cart' not in session:
+        print("Cart DATA Not Present ")
+        session['cart'] = []  # Initialize cart if not exists in session
+        session['cart'].append({'id': game_info['id'], 'name':game_info['game_name'], 'price':game_info['base_price'] })  # Add the gameId to the cart
+    else:
+        print("Cart DATA  Present ",session['cart'])
+        print("Adding More in Session",game_id)
+        temp = session['cart']
+        temp.append({'id': game_info['id'], 'name':game_info['game_name'], 'price':game_info['base_price'] })
+        session['cart']= temp  # Add the gameId to the cart
+    return jsonify({'message': 'Game added to cart successfully'}), 200 
+
+
+@app.route('/remove_from_cart', methods=['POST'])
+def remove_from_cart():
+    game_id = request.json.get('gameId')
+    print("Here ??",game_id)
+
+    cart = session['cart']
+    for item in cart:
+        print(item['id'] == game_id)
+        if item['id'] == game_id:
+            cart.remove(item)
+            session['cart'] = cart
+            return jsonify({'message': 'Game removed from cart successfully'}), 200            
+        else:
+            return jsonify({'error': 'Game ID not found in cart'}), 404  
+
+
+
+
+
 
 
 
